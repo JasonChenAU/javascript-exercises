4e6cb42ed312abf79734fe350f6d23039bb61067
JasonChenAU <169026241+JasonChenAU@users.noreply.github.com>
2025-02-26 23:53:03 +1100

    test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,13 +1,15 @@
 const repeatString = function(word, times) {
-    //return console.log(`${word.repeat(times)}`);
-    if (times < 0) return "ERROR";
-    let string = "";
-    for (let i = 0; i < times; i++) {
-      string += word;
-    }
-    return string;
+    return word.repeat(times);
+    // if (times < 0) return "ERROR";
+    // let string = "";
+    // for (let i = 0; i < times; i++) {
+    //   string += word;
+    // }
+    // return string;
 
 };
+
+console.log(repeatString("test", 3));
 
 // Do not edit below this line
 export default repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let newStr = "";
+    for (let i = input.length-1; i >= 0; i--) {
+        newStr += input[i];
+    }
+    return newStr;
 };
+
+console.log(reverseString("test"));
 
 // Do not edit below this line
 module.exports = reverseString;
